31de7280f88614ce476699933f3b43d50d138e8b
Fouche du Preez <fouche@limehouse.co.za>
2023-03-20 08:06:51 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+//    return view('welcome');
+    $redis = app()->make('redis');
+    $redis->set('key1', 'testValue');
+    return $redis->get('key1');
+});
+
